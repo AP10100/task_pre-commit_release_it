4a299d6528253ba74fd58fb80195af7476d658c2
AP <amanpatel34677@gmail.com>
2023-12-18 17:48:51 +0530

    updated

--- sample.js
+++ sample.js
@@ -2,5 +2,5 @@ const name = 'James'
 const person = {first: name}
 console.log(person)
 const sayHelloLinting = (fName) => {
-console.log(`Hello linting, ${fName  }`);
+console.log(`Hello linting, ${fName }`);
 };
